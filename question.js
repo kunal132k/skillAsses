4d8dcde8629aab798f6f8ca9ebe4af29b222900a
kunal132k <kunal132k@gmail.com>
2021-04-24 15:09:40 +0530

    final commit

---
+++ question.js
@@ -0,0 +1,142 @@
+let questions = [
+    {
+        numb: 1,
+        topic: "HTML",
+        question: "  What does HTML stands for?",
+        answer: "Hypertext Markup Language.",
+        options: ['Hypertext Machine language.', 'Hypertext Machine language.', 'Hypertext Markup Language.', 'Hightext machine language.']
+    },
+    {
+        numb: 2,
+        topic: "HTML",
+        question: " The correct sequence of HTML tags for starting a webpage is -",
+        answer: "HTML, Head, Title, Body",
+        options: ['Head, Title, HTML, body', 'HTML, Body, Title, Head', 'HTML, Title,  Head, Body', 'HTML, Head, Title, Body']
+    },
+    {
+        numb: 3,
+        topic: "HTML",
+        question: "  The hr tag in HTML is used for -",
+        answer: "horizontal ruler",
+        options: ['new line', 'vertical ruler', 'new paragraph', 'horizontal ruler']
+    },
+    {
+        numb: 4,
+        topic: "HTML",
+        question: "Which of the following attribute is used to provide a unique name to an element?",
+        answer: "id",
+        options: ['class', 'id', 'type', 'none']
+    },
+    {
+        numb: 5,
+        topic: "CSS",
+        question: "  What does CSS stand for?",
+        answer: "Cascading style sheets",
+        options: ['Cascade style sheets', 'Color and style sheets', 'Cascading style sheets', ' None']
+    },
+    {
+        numb: 6,
+        topic: "CSS",
+        question: "  The property in CSS used to change the background color of an element is -",
+        answer: "background-color",
+        options: ['bgcolor', 'color', 'background-color', 'All of the above']
+    },
+    {
+        numb: 7,
+        topic: "CSS",
+        question: "  The CSS property used to control the element's font-size is -",
+        answer: "font-size",
+        options: ['text-style', 'text-size', 'font-size', 'None of the above']
+    },
+    {
+        numb: 8,
+        topic: "CSS",
+        question: "  Are the negative values allowed in padding property?",
+        answer: "No",
+        options: ['Yes', 'No', 'May be', 'Can not say']
+    },
+    {
+        numb: 9,
+        topic: "javascript",
+        question: "  Which type of language JavaScript is ___",
+        answer: "Object-Based",
+        options: ['Object-Oriented', 'Object-Based', 'Assembly-language', 'High-level']
+    },
+    {
+        numb: 10,
+        topic: "javascript",
+        question: "  Which one of the following is not a keyword:",
+        answer: "use strict",
+        options: ['if', 'with', 'debugger', 'use strict']
+    },
+    {
+        numb: 11,
+        topic: "javascript",
+        question: " Which one of the following method or operator is used for identification of the array?",
+        answer: "isarrayType()",
+        options: ['Typeof', '==', '===' , 'isarrayType()']
+    },
+    {
+        numb: 12,
+        topic: "javascript",
+        question: " In JavaScript, what kind of scoping is used?",
+        answer: "Lexical scoping",
+        options: ['Lexical scoping', 'Literal scoping', 'Sequential scoping', 'Segmental scoping']
+    },
+    {
+        numb: 13,
+        topic: "php",
+        question: " The term PHP is an acronym for PHP:_______________",
+        answer: "Hypertext Preprocessor",
+        options: ['Hypertext Preprocessor', 'Hypertext multiprocessor', 'Hypertext markup Preprocessor', 'Hypertune Preprocessor']
+    },
+    {
+        numb: 14,
+        topic: "php",
+        question: " PHP is a ____________ language?",
+        answer: "server-side scripting",
+        options: ['user-side scripting', 'client-side scripting', 'server-side scripting', 'None']
+    },
+    {
+        numb: 15,
+        topic: "php",
+        question: " PHP files have a default file extension of_______",
+        answer: "php",
+        options: ['html', 'xml', 'php', 'hphp']
+    },
+    {
+        numb: 16,
+        topic: "php",
+        question: " In which year php was created?",
+        answer: "1994",
+        options: ['1993', '1994', '1996', '1999']
+    },
+    {
+        numb: 17,
+        topic: "Node",
+        question: " All APIs of Node.JS are",
+        answer: "Asynchronous",
+        options: ['Asynchronous', 'Synchronous', 'Both', 'None']
+    },
+    {
+        numb: 18,
+        topic: "Node",
+        question: " Which of the following is true about readable stream?",
+        answer: "Both",
+        options: ['Readable stream is used for read operation', 'Output of readable stream can be input to a writable stream', 'Both', 'None']
+    },
+    {
+        numb: 19,
+        topic: "Node",
+        question: " Which of the following command will show all the modules installed globally?",
+        answer: "$ npm ls -g",
+        options: ['$ npm ls -g', '$ npm ls', '$ node ls -g', '$ node ls']
+    },
+    {
+        numb: 20,
+        topic: "Node",
+        question: " Which of the following code can make a request to a web server? ",
+        answer: "http.request(options, callback)",
+        options: ['http.request(options, callback)', 'http.createRequest(options, callback)', 'http.makeRequest(options, callback)', 'None']
+    },
+];
